cb00e0bf5af25c82f0e68a82bec05d1aae87a3fa
Jonathan <Jonathan.jadebondkoivumaa@elev.ga.ntig.se>
2024-10-22 10:13:32 +0200

    La till en ny karaktär

--- halloween-program.py
+++ halloween-program.py
@@ -1,7 +1,7 @@
 # halloween.py
 
 # Fråga användaren om deras favorit-Halloween-karaktär
-karaktar = input("Vilken är din favorit-brainrot-karaktär? (varulv, vatten, mango, ohio): ").lower()
+karaktar = input("Vilken är din favorit-brainrot-karaktär? (varulv, vatten, mango, ohio, rizzler): ").lower()
 
 # Använd en if-sats för att ge ett svar baserat på användarens val
 if karaktar == "varulv":
@@ -12,5 +12,7 @@ elif karaktar == "mango":
     print("Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein")
 elif karaktar == "ohio":
     print("Only in ohio")
+elif karaktar == "Rizzler:
+    print("W rizz")
 else:
     print("Det låter som en skibidi karaktär!")
