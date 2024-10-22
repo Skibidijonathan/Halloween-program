b199c62d5a461459a82888b22e5fc0622c992ccc
Jonathan <Jonathan.jadebondkoivumaa@elev.ga.ntig.se>
2024-10-22 10:10:33 +0200

    Tog bort de originella karaktärerna. fixade en ny karaktär.

--- halloween-program.py
+++ halloween-program.py
@@ -1,20 +1,16 @@
 # halloween.py
 
 # Fråga användaren om deras favorit-Halloween-karaktär
-karaktar = input("Vilken är din favorit-Halloween-karaktär? (vampyr, zombie, spöke, varulv, vatten, mango): ").lower()
+karaktar = input("Vilken är din favorit-brainrot-karaktär? (varulv, vatten, mango, ohio): ").lower()
 
 # Använd en if-sats för att ge ett svar baserat på användarens val
-if karaktar == "vampyr":
-    print("Blod är livet!")
-elif karaktar == "zombie":
-    print("Hjärnor... hjärnor...")
-elif karaktar == "spöke":
-    print("Buuuu!")
-elif karaktar == "varulv":
+if karaktar == "varulv":
     print("Du är sigma lone wolf")
 elif karaktar == "vatten":
     print("dop dop")
 elif karaktar == "mango":
     print("Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein Mango fein")
+elif karaktar == "ohio":
+    print("Only in ohio")
 else:
-    print("Det låter som en spännande karaktär!")
+    print("Det låter som en skibidi karaktär!")
